0fd311107c3b8b7c9b40ad40e14e15c70fb00f4a
manumoncho <moncholamas@gmail.com>
2021-02-11 11:55:58 -0600

    Inicio del proyecto en VS

---
+++ Teyser-Gestor/Teyser-Gestor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teyser_Gestor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
