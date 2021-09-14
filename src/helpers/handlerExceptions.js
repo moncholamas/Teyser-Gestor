93659e847e04a0ed7c894b495ca489b00d254883
Manuel Lamas <moncholamas@gmail.com>
2021-09-14 11:40:54 -0300

    agregado log4js para logs en produccion y desarrollo

--- src/helpers/handlerExceptions.js
+++ src/helpers/handlerExceptions.js
@@ -2,11 +2,18 @@
 // desarrollo -> excepciones por consol.log
 // produccion -> por correo electronico
 require('dotenv').config();
+const log4js = require("log4js");
+const logger = log4js.getLogger();
+logger.level = "debug";
+
+export {logger};
+
 
 export function handlerException(error){
     if(process.env.ENV==='produccion'){
-        console.error(error);
+        logger.error(error);
     }else{
-        console.log('temandamos un correo');
+        logger.error(error);
+        //implementar el envio del log al correo con NODEMAILER
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import app from './app.js';
 import { sequelize } from './db/db.js';
-import { handlerException } from './helpers/handlerExceptions'
-
+import { handlerException } from './helpers/handlerExceptions';
+import {logger} from './helpers/handlerExceptions'
+ 
 
 async function main(){
      try {
         const port = process.env.PORT || 3009;
         app.listen(port);
         await sequelize.authenticate();
-        console.log(`servidor corriendo en el puerto: ${port}`);
+        logger.info(`servidor corriendo en el puerto: ${port}`);
      }catch(e){
          handlerException(e);
      }
