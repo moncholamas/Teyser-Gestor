c82b3ba781dbe33f6c1e3953a1d27a63cc180f1c
Manuel Lamas <moncholamas@gmail.com>
2021-10-15 09:24:37 -0300

    2da correccion a la conexion para deploy en heroku

--- src/db/db.js
+++ src/db/db.js
@@ -3,25 +3,20 @@ import { dataBase } from '../config'
 
 const {userName, DB ,password,puertoDb,host} = dataBase;
 
-const sequelize = new Sequelize(`postgres://${userName}:${password}@${host}:${puertoDb}/${DB}`);
 
-/*
- const sequelize = new Sequelize(
-    dataBase.dataBase, //db
-    dataBase.userName, //username
-    dataBase.password, //password
-    {
-        host: dataBase.host, //server
-        dialect: 'postgres',
-        pool: {
-            max: 5,
-            min: 0,
-            require: 30000,
-            idle: 10000
-        },
-        logging: false
-    }
-)
-*/
+const sequelize = new Sequelize({
+    database: DB,
+    username: userName,
+    password,
+    host,
+    port: puertoDb,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+      }
+    },
+  });
 
 export { sequelize };
